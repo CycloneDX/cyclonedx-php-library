8da305e28fa8ff4ff25d529be817a1b4cc3b5c3f
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-02-13 12:03:24 +0100

    schema-downloader-improv
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tools/schema-downloader/download.php
+++ tools/schema-downloader/download.php
@@ -35,42 +35,58 @@ namespace tools\CycloneDX\SchemaDownloader;
 const SOURCE_ROOT = 'https://raw.githubusercontent.com/CycloneDX/specification/master/schema/';
 const TARGET_ROOT = __DIR__.'/../../res/';
 
-abstract class BomXsd
+abstract class BaseDownloadable
 {
-    public const Versions = ['1.0', '1.1', '1.2', '1.3', '1.4'];
-    public const SourcePattern = SOURCE_ROOT.'bom-%s.xsd';
-    public const TargetPattern = TARGET_ROOT.'bom-%s.SNAPSHOT.xsd';
-    public const Replace = [
+    /** @psalm-var list<string> */
+    public const Versions = [];
+
+    /** @var string */
+    public const SourcePattern = SOURCE_ROOT.'...';
+
+    /** @var string */
+    public const TargetPattern = TARGET_ROOT.'...';
+
+    /** @psalm-var array<string, string>  */
+    public const Replace = [];
+}
+
+abstract class BomXsd extends BaseDownloadable
+{
+    final public const Versions = ['1.0', '1.1', '1.2', '1.3', '1.4'];
+    final public const SourcePattern = SOURCE_ROOT.'bom-%s.xsd';
+    final public const TargetPattern = TARGET_ROOT.'bom-%s.SNAPSHOT.xsd';
+    final public const Replace = [
         'schemaLocation="http://cyclonedx.org/schema/spdx"' => 'schemaLocation="spdx.SNAPSHOT.xsd"',
         'schemaLocation="https://cyclonedx.org/schema/spdx"' => 'schemaLocation="spdx.SNAPSHOT.xsd"',
     ];
 }
 
-abstract class BomJsonLax
+abstract class BomJsonLax extends BaseDownloadable
 {
-    public const Versions = ['1.2', '1.3', '1.4'];
-    public const SourcePattern = SOURCE_ROOT.'bom-%s.schema.json';
-    public const TargetPattern = TARGET_ROOT.'bom-%s.SNAPSHOT.schema.json';
-    public const Replace = [
+    final public const Versions = ['1.2', '1.3', '1.4'];
+    final public const SourcePattern = SOURCE_ROOT.'bom-%s.schema.json';
+    final public const TargetPattern = TARGET_ROOT.'bom-%s.SNAPSHOT.schema.json';
+    final public const Replace = [
         'spdx.schema.json' => 'spdx.SNAPSHOT.schema.json',
         'jsf-0.82.schema.json' => 'jsf-0.82.SNAPSHOT.schema.json',
     ];
 }
 
-abstract class BomJsonStrict
+abstract class BomJsonStrict extends BaseDownloadable
 {
-    public const Versions = ['1.2', '1.3'];
-    public const SourcePattern = SOURCE_ROOT.'bom-%s-strict.schema.json';
-    public const TargetPattern = TARGET_ROOT.'bom-%s-strict.SNAPSHOT.schema.json';
-    public const Replace = BomJsonLax::Replace;
+    final public const Versions = ['1.2', '1.3'];
+    final public const SourcePattern = SOURCE_ROOT.'bom-%s-strict.schema.json';
+    final public const TargetPattern = TARGET_ROOT.'bom-%s-strict.SNAPSHOT.schema.json';
+    final public const Replace = BomJsonLax::Replace;
 }
 
-const Others = [
+const OtherDownloadables = [
     SOURCE_ROOT.'spdx.schema.json' => TARGET_ROOT.'spdx.SNAPSHOT.schema.json',
     SOURCE_ROOT.'spdx.xsd' => TARGET_ROOT.'spdx.SNAPSHOT.xsd',
     SOURCE_ROOT.'jsf-0.82.schema.json' => TARGET_ROOT.'jsf-0.82.SNAPSHOT.schema.json',
 ];
 
+/** @psalm-var class-string<BaseDownloadable>  $class */
 foreach ([
              BomXsd::class,
              BomJsonLax::class,
@@ -83,9 +99,10 @@ foreach ([
         $content = file_get_contents($source);
         $content = strtr($content, $class::Replace);
         file_put_contents($target, $content);
+        unset($content);
     }
 }
 
-foreach (Others as $source => $target) {
+foreach (OtherDownloadables as $source => $target) {
     file_put_contents($target, file_get_contents($source));
 }
