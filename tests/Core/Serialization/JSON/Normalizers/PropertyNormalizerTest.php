be83f634ba71788393b45855fa3115eac811f909
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-02-19 12:57:19 +0100

    more tests
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ tests/Core/Serialization/JSON/Normalizers/PropertyNormalizerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of CycloneDX PHP Library.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) OWASP Foundation. All Rights Reserved.
+ */
+
+namespace CycloneDX\Tests\Core\Serialization\JSON\Normalizers;
+
+use CycloneDX\Core\Models\Property;
+use CycloneDX\Core\Serialization\JSON\_BaseNormalizer;
+use CycloneDX\Core\Serialization\JSON\NormalizerFactory;
+use CycloneDX\Core\Serialization\JSON\Normalizers\PropertyNormalizer;
+use CycloneDX\Core\Spec\Spec;
+use DomainException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PropertyNormalizer::class)]
+#[UsesClass(_BaseNormalizer::class)]
+class PropertyNormalizerTest extends TestCase
+{
+    public function testNormalizeFull(): void
+    {
+        $property = $this->createConfiguredMock(
+            Property::class,
+            [
+                'getName' => 'myName',
+                'getValue' => 'myValue',
+            ]
+        );
+        $spec = $this->createMock(Spec::class);
+        $factory = $this->createConfiguredMock(NormalizerFactory::class, ['getSpec' => $spec]);
+        $normalizer = new PropertyNormalizer($factory);
+
+        $actual = $normalizer->normalize($property);
+
+        self::assertSame(
+            [
+                'name' => 'myName',
+                'value' => 'myValue',
+            ],
+            $actual
+        );
+    }
+
+    public function testNormalizeNoNameThrows(): void
+    {
+        $property = $this->createConfiguredMock(
+            Property::class,
+            [
+                'getName' => '',
+                'getValue' => 'myValue',
+            ]
+        );
+        $spec = $this->createMock(Spec::class);
+        $factory = $this->createConfiguredMock(NormalizerFactory::class, ['getSpec' => $spec]);
+        $normalizer = new PropertyNormalizer($factory);
+
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessageMatches('/name/');
+
+        $normalizer->normalize($property);
+    }
+}
---
+++ tests/Core/Serialization/JSON/Normalizers/PropertyRepositoryNormalizerTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of CycloneDX PHP Library.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) OWASP Foundation. All Rights Reserved.
+ */
+
+namespace CycloneDX\Tests\Core\Serialization\JSON\Normalizers;
+
+use CycloneDX\Core\Collections\PropertyRepository;
+use CycloneDX\Core\Models\Property;
+use CycloneDX\Core\Serialization\JSON\_BaseNormalizer;
+use CycloneDX\Core\Serialization\JSON\NormalizerFactory;
+use CycloneDX\Core\Serialization\JSON\Normalizers\PropertyNormalizer;
+use CycloneDX\Core\Serialization\JSON\Normalizers\PropertyRepositoryNormalizer;
+use CycloneDX\Core\Spec\Spec;
+use DomainException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PropertyRepositoryNormalizer::class)]
+#[CoversClass(_BaseNormalizer::class)]
+#[UsesClass(PropertyNormalizer::class)]
+class PropertyRepositoryNormalizerTest extends TestCase
+{
+    public function testNormalizeEmpty(): void
+    {
+        $spec = $this->createStub(Spec::class);
+        $propertyNormalizer = $this->createMock(PropertyNormalizer::class);
+        $factory = $this->createConfiguredMock(NormalizerFactory::class, [
+            'getSpec' => $spec,
+            'makeForProperty' => $propertyNormalizer,
+        ]);
+
+        $normalizer = new PropertyRepositoryNormalizer($factory);
+        $properties = $this->createConfiguredMock(PropertyRepository::class, ['count' => 0]);
+
+        $actual = $normalizer->normalize($properties);
+
+        self::assertSame([], $actual);
+    }
+
+    public function testNormalize(): void
+    {
+        $spec = $this->createStub(Spec::class);
+        $propertyNormalizer = $this->createMock(PropertyNormalizer::class);
+        $factory = $this->createConfiguredMock(NormalizerFactory::class, [
+            'getSpec' => $spec,
+            'makeForProperty' => $propertyNormalizer,
+        ]);
+        $normalizer = new PropertyRepositoryNormalizer($factory);
+        $property = $this->createStub(Property::class);
+        $properties = $this->createConfiguredMock(PropertyRepository::class, [
+            'count' => 1,
+            'getItems' => [$property],
+        ]);
+
+        $propertyNormalizer->expects(self::once())->method('normalize')
+            ->with($property)
+            ->willReturn(['FakeProperty' => 'dummy']);
+
+        $actual = $normalizer->normalize($properties);
+
+        self::assertSame([['FakeProperty' => 'dummy']], $actual);
+    }
+
+    public function testNormalizeSkipWHenThrown(): void
+    {
+        $spec = $this->createStub(Spec::class);
+        $propertyNormalizer = $this->createMock(PropertyNormalizer::class);
+        $factory = $this->createConfiguredMock(NormalizerFactory::class, [
+            'getSpec' => $spec,
+            'makeForProperty' => $propertyNormalizer,
+        ]);
+        $normalizer = new PropertyRepositoryNormalizer($factory);
+        $property = $this->createStub(Property::class);
+        $properties = $this->createConfiguredMock(PropertyRepository::class, [
+            'count' => 1,
+            'getItems' => [$property],
+        ]);
+
+        $propertyNormalizer->expects(self::once())->method('normalize')
+            ->with($property)
+            ->willThrowException(new DomainException());
+
+        $actual = $normalizer->normalize($properties);
+
+        self::assertSame([], $actual);
+    }
+}
