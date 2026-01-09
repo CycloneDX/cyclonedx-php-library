c134bb48ba0d4b606114db6c69997859d4715623
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-01-09 13:32:53 +0100

    examples
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- examples/build_and_serialize.php
+++ examples/build_and_serialize.php
@@ -63,7 +63,7 @@ $serializedJSON = (new \CycloneDX\Core\Serialization\JsonSerializer(
     new \CycloneDX\Core\Serialization\JSON\NormalizerFactory($spec)
 ))->serialize($bom, $prettyPrint);
 echo $serializedJSON, \PHP_EOL;
-$jsonValidationErrors = (new \CycloneDX\Core\Validation\Validators\JsonValidator($spec))->validateString($serializedJSON);
+$jsonValidationErrors = (new \CycloneDX\Core\Validation\Validators\JsonValidator($spec->getVersion()))->validateString($serializedJSON);
 if (null === $jsonValidationErrors) {
     echo 'JSON valid', \PHP_EOL;
 } else {
@@ -76,7 +76,7 @@ $serializedXML = (new \CycloneDX\Core\Serialization\XmlSerializer(
     new \CycloneDX\Core\Serialization\DOM\NormalizerFactory($spec)
 ))->serialize($bom, $prettyPrint);
 echo $serializedXML, \PHP_EOL;
-$xmlValidationErrors = (new \CycloneDX\Core\Validation\Validators\XmlValidator($spec))->validateString($serializedXML);
+$xmlValidationErrors = (new \CycloneDX\Core\Validation\Validators\XmlValidator($spec->getVersion()))->validateString($serializedXML);
 if (null === $xmlValidationErrors) {
     echo 'XML valid', \PHP_EOL;
 } else {
